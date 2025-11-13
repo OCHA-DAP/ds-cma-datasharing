6a6bab7f585e6e1a9a83bf268e683ab7bfde5e84
Tristan Downing <downing.tristan@gmail.com>
2025-11-12 17:30:18 -0800

    try python 3

--- App_Data/jobs/triggered/chd-ftp-job/run.cmd
+++ App_Data/jobs/triggered/chd-ftp-job/run.cmd
@@ -1,2 +1,6 @@
 @echo off
-python D:\home\site\wwwroot\pipelines\test_simple_chd_ftp.py
+REM Show which Python version we’re using (will appear in logs)
+py -3 -c "import sys; print(sys.version)"
+
+REM Run your script with Python 3
+py -3 D:\home\site\wwwroot\pipelines\test_simple_chd_ftp.py
