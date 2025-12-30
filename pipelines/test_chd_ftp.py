19b5e37783fbd1278460f8bc8617a50bc70c3851
Tristan Downing <downing.tristan@gmail.com>
2025-12-30 09:15:21 -0800

    switch to ftp tls

--- pipelines/test_chd_ftp.py
+++ pipelines/test_chd_ftp.py
@@ -1,13 +1,20 @@
 import os
-from ftplib import FTP
+
+# from ftplib import FTP
+from ftplib import FTP_TLS
 
 host = os.environ["FTP_HOST"]
 user = os.environ["FTP_USER"]
 pwd = os.environ["FTP_PASS"]
 
 # Connect to plain FTP (no TLS)
-ftp = FTP(host)
+# ftp = FTP(host)
+# ftp.login(user=user, passwd=pwd)
+
+ftp = FTP_TLS()
+ftp.connect(host, 21)
 ftp.login(user=user, passwd=pwd)
+ftp.prot_p()  # switch to secure data connection
 
 print("Connected to:", host)
 print("Current directory:", ftp.pwd())
