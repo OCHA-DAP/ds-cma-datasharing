1accf21796897f4aca10d9b14da5d92f77609e25
Tristan Downing <downing.tristan@gmail.com>
2025-12-31 14:34:31 -0800

    user install

--- App_Data/jobs/triggered/chd-ftp-job/run.cmd
+++ App_Data/jobs/triggered/chd-ftp-job/run.cmd
@@ -3,8 +3,8 @@ REM Show Python version
 py -3 -c "import sys; print(sys.version)"
 
 REM Install dependencies
-py -3 -m pip install --upgrade pip
-py -3 -m pip install -r D:\home\site\wwwroot\requirements.txt
+py -3 -m pip install --user --upgrade pip
+py -3 -m pip install --user -r D:\home\site\wwwroot\requirements.txt
 
 REM Run the script
 py -3 D:\home\site\wwwroot\pipelines\test_sftp.py
