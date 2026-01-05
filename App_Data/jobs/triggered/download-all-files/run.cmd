5a69c60efd4d777c60d39e54cac9653a7f6ad224
Tristan Downing <downing.tristan@gmail.com>
2026-01-05 11:57:44 -0800

    undo suppress

--- App_Data/jobs/triggered/download-all-files/run.cmd
+++ App_Data/jobs/triggered/download-all-files/run.cmd
@@ -7,5 +7,4 @@ py -3 -m pip install --user --upgrade pip
 py -3 -m pip install --user --no-warn-script-location -r D:\home\site\wwwroot\pipeline_requirements.txt
 
 REM Run the script
-set PYTHONWARNINGS=ignore
 py -3 D:\home\site\wwwroot\pipelines\download_all_files.py
