702a99eadf4f3322706209eca7c79036eeb0ee6b
Tristan Downing <downing.tristan@gmail.com>
2026-01-05 11:56:40 -0800

    suppress all warnings

--- App_Data/jobs/triggered/download-all-files/run.cmd
+++ App_Data/jobs/triggered/download-all-files/run.cmd
@@ -7,4 +7,5 @@ py -3 -m pip install --user --upgrade pip
 py -3 -m pip install --user --no-warn-script-location -r D:\home\site\wwwroot\pipeline_requirements.txt
 
 REM Run the script
+set PYTHONWARNINGS=ignore
 py -3 D:\home\site\wwwroot\pipelines\download_all_files.py
